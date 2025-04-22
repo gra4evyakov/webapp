04879fec63d19a697d3a9110ee3d88e7525bcea4
gra4evyakov <gra4evyakov@yandex.ru>
2025-04-22 20:52:22 +0400

    titles

--- src/components/common/common.info.vue
+++ src/components/common/common.info.vue
@@ -20,15 +20,16 @@ defineProps({
 .common-info {
   display: flex;
   flex-direction: column;
-  gap: 8px;
+  gap: 12px;
 
   h1 {
-    font-size: 32px;
+    font-size: 28px;
     font-weight: 700;
+    white-space: pre-line;
   }
 
   p {
-    font-size: 16px;
+    font-size: 14px;
     white-space: pre-line;
   }
 }
